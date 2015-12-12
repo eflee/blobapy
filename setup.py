403347d5ce8505da40f758ce5418dfefca8930c4
Joe Cross <joe.mcross@gmail.com>
2015-12-12 13:20:05 -0800

    Generic project skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+""" Setup file """
+from setuptools import setup, find_packages
+
+def get_version():
+    with open("blobapy/__init__.py") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1])
+
+REQUIREMENTS = [
+]
+
+TEST_REQUIREMENTS = [
+    "coverage",
+    "flake8",
+    "pytest",
+    "tox",
+]
+
+if __name__ == "__main__":
+    setup(
+        name="blobapy",
+        version=get_version(),
+        url="https://github.com/eflee/blobapy",
+        packages=find_packages(exclude=("tests", "docs", "examples")),
+        install_requires=REQUIREMENTS,
+        tests_require=REQUIREMENTS + TEST_REQUIREMENTS,
+    )
